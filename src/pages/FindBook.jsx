b34085db529a55cc57a2835587f0014ae77cb825
Deki1973 <konidek37@gmail.com>
2024-03-21 22:49:31 +0100

    fix client issues dealing finding a single book

--- src/pages/FindBook.jsx
+++ src/pages/FindBook.jsx
@@ -1,55 +1,54 @@
 import "@style/_FindBook.scss";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSingleBook } from "../context/SingleBookContext";
-
+import BookCard from "../components/BookCard";
 
 const FindBook = () => {
+  const { singleBook, setTitleInput } = useSingleBook();
+  const [text, setText] = useState("");
 
-    const {singleBook,setTitleInput}=useSingleBook();
+  useEffect(() => {
+    console.log(singleBook);
+  }, [singleBook]);
 
-    const handleSubmit=async (e)=>{
-        e.preventDefault();
-        const bookTitle=e.target[0].value;
-        console.log(bookTitle);
-        try{
-
-            setTitleInput(bookTitle);
-            console.log(singleBook);
-            /*
-            alert(bookTitle);
-            const res = await axios(
-                {
-                  method: "post",
-                  url:  "http://localhost:5000/books/find",
-                  data: {title:bookTitle},
-                }
-              );
-    
-                if(res.ok){
-                    console.log(res.data)
-    
-                }
-        */
-        }catch(error){
-            console.log(error);
-        }
-        
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setTitleInput(text);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-
-    return ( 
-    
-        <div className="find-book-container">
-            <h1>Nadji knjigu po naslovu</h1>
-            <form action="submit" onSubmit={handleSubmit}>
-            <input type="text" id="bookTitle" name="bookTitle" className="book-title"/>
-            <button type="submit">NADJI</button>
-            </form>
+  return (
+    <div className="find-book-container">
+      <h1>Nadji knjigu po naslovu</h1>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="bookTitle"
+          name="bookTitle"
+          className="book-title"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button type="submit">NADJI</button>
+      </form>
+      {singleBook != null && (
+        <div className="book-card">
+          <BookCard
+            _id={singleBook?._id}
+            title={singleBook?.title}
+            author={singleBook?.author}
+            description={singleBook?.description}
+            image={singleBook?.image}
+          />
         </div>
-    
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default FindBook;
